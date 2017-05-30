edf07a6e3a72a2ad98d5533d0c5f56f6b0969e6c
Piotr Kozubek <piotrkozubek@gmail.com>
2017-05-30 15:56:00 +0200

    Update

---
+++ Exercise3/ex3.py
@@ -0,0 +1,21 @@
+# just text
+print "I will now count my chickens:"
+# counting hens
+print "Hens", 25 + 30 /6
+#counting roosters
+print "Roosters", 100 - 25 * 3 % 4
+#counting eggs
+print "Now I will count the eggs:"
+# eggs calculation
+print 3+2+1-5+4%2-1/4+6
+# more parameters
+print "Is it true than 3+2 < 5 -7?"
+# checking equlity
+print 3+2 < 5-7
+print "What is 3+2?", 3+2
+print "What is 5 - 7?", 5-7
+print "Oh, that's why it's False."
+print "How about some more."
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5>=-2
+print "Is it less or equal?", 5<=-2
---
+++ Exercise5/ex5.py
@@ -0,0 +1,15 @@
+name = "Zed A. Shaw"
+age = 30
+height = 182
+weight = 76
+eyes = "Blue"
+teeth = "white"
+hair = "Dark"
+
+print "Let's talk about %s." % name
+print "He's %d cm tall." % height
+print "He's %d kg heavy." % weight
+print "Actually it's not that heavy"
+print "He's got %s eyes and %s hair" % (eyes, hair)
+print "His teeth are usually %s depending on the coffee" % teeth
+print "If I add %d, %d, and %d I get %d" % (age, height, weight, age + height + weight)
