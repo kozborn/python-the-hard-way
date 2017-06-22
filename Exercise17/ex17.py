9ad9e1711acf3f51d527dd62d6f2dbc7f5c13805
Piotr Kozubek <piotrkozubek@gmail.com>
2017-06-22 13:58:55 +0200

    UPdate

--- Exercise17/ex17.py
+++ Exercise17/ex17.py
@@ -17,3 +17,22 @@ in_file.close()
 out_file.close()
 
 
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+print "Copying from %s to %s" % (from_file, to_file)
+in_file = open(from_file)
+indata = in_file.read()
+
+print "The input file is %d bytes long" % len(indata)
+print "Does the output file exists? %r" % exists(to_file)
+print "Ready, hit ENTER to continue, CTRL+C to abort"
+raw_input()
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+print "Allright, all done"
+out_file.close()
+in_file.close()
